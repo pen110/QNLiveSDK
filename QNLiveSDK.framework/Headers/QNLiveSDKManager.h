f9b2144c19a41472a8552b8e32b2894b6c154e45
px <956707454@qq.com>
2022-08-31 10:29:01 +0800

    share_id

--- QNLiveSDK.framework/Headers/QNLiveSDKManager.h
+++ QNLiveSDK.framework/Headers/QNLiveSDKManager.h
@@ -177,10 +177,9 @@ typedef void ( ^successBloc) (id content);
 /// @param failure failure description
 - (void)getRankingGift:(NSString *)params successBloc:(successBloc)successBlock failure:(void (^)(NSError * _Nonnull error))failure;
 
-/// 添加一个分享的链接，返回一个100*100 的image
+/// 返回一个100*100 的image
 /// liveRoomInfo: error 此方法中返回share_card 和share_url
-/// @param url url description
-- (UIImage *)setupQRCodeImage:(NSString *)url;
+- (UIImage *)setupQRCodeImage;
 
 /**
  直播间信息，点赞，排行榜
